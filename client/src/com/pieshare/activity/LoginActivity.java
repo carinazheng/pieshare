2d9faac9eb20d8ce8d87b6e89912c3f86a2fa6c6
yingliu <yingliu@fb.com>
2012-06-17 18:21:39 -0700

    Finish login in android.

--- client/src/com/pieshare/activity/LoginActivity.java
+++ client/src/com/pieshare/activity/LoginActivity.java
@@ -32,6 +32,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.util.Log;
 
+import com.pieshare.util.PSHttpClient;
+
 public class LoginActivity extends Activity {
 	private static final String TAG = "LoginActivity";
 	
@@ -44,80 +46,31 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login);
         
         Button btnLogin = (Button)findViewById(R.id.buttonLogin);
-        EditText txtUsername = (EditText)findViewById(R.id.editTextUsername);
-        EditText txtPassword = (EditText)findViewById(R.id.editTextPassword);
+
         
         btnLogin.setOnClickListener(new OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		DefaultHttpClient httpclient = new DefaultHttpClient();
-
-        	    try {
-        	        HttpGet httpget = new HttpGet(LOGIN_URL);
-        	      try{
-        	        HttpResponse response = httpclient.execute(httpget);
-        	        HttpEntity entity = response.getEntity();
-
-        	        //  System.out.println("Login form get: " + response.getStatusLine());
-        	        //  EntityUtils.consume(entity);
-
-        	        // System.out.println("Initial set of cookies:");
-        	        List<Cookie> cookies = httpclient.getCookieStore().getCookies();
-        	        if (cookies.isEmpty()) {
-        	            Log.d(TAG, "None");
-        	        } else {
-        	            for (int i = 0; i < cookies.size(); i++) {
-        	            	Log.d(TAG, "- " + cookies.get(i).toString());
-        	            }
-        	        }
-        	        
-        	        HttpPost httpost = new HttpPost(LOGIN_URL);
-
-        	        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-        	        nvps.add(new BasicNameValuePair(LOGIN_USER_FIELD, "sb"));
-        	        nvps.add(new BasicNameValuePair(LOGIN_PASS_FIELD, "sb"));
-
-        	        try {
-        	            httpost.setEntity(new UrlEncodedFormEntity(nvps));
-        	        } catch (UnsupportedEncodingException e) {
-        	            // TODO Auto-generated catch block
-        	            e.printStackTrace();
-        	        }
-
-        	        try {
-        	            response = httpclient.execute(httpost);
-        	        } catch (ClientProtocolException e) {
-        	            // TODO Auto-generated catch block
-        	            e.printStackTrace();
-        	        } catch (IOException e) {
-        	            // TODO Auto-generated catch block
-        	            e.printStackTrace();
-        	        }
-        	        entity = response.getEntity();
-        	        Toast.makeText(getBaseContext(), "done",Toast.LENGTH_LONG);
-        	        Log.d(TAG, "Login form get: " + response.getStatusLine());
-        	    //   EntityUtils.consume(entity);
-
-        	        Log.d(TAG, "Post logon cookies:");
-        	        cookies = httpclient.getCookieStore().getCookies();
-        	        if (cookies.isEmpty()) {
-        	        	Log.d(TAG, "None");
-        	        } else {
-        	            for (int i = 0; i < cookies.size(); i++) {
-        	            	Log.d(TAG, "- " + cookies.get(i).toString());
-        	            }
-        	        }
-
-        	    }finally {} }catch (Exception e) {
-        	        // TODO: handle exception
-        	    	Log.d(TAG, e.toString());
-        	    }finally {
-        	        // When HttpClient instance is no longer needed,
-        	        // shut down the connection manager to ensure
-        	        // immediate deallocation of all system resources
-        	        httpclient.getConnectionManager().shutdown();
-        	        Toast.makeText(getBaseContext(), "done",Toast.LENGTH_LONG);
-        	    }
+                EditText txtUsername = (EditText)findViewById(R.id.editTextUsername);
+                EditText txtPassword = (EditText)findViewById(R.id.editTextPassword);
+                
+        		HttpClient httpClient = PSHttpClient.getHttpClient();
+        		if (PSHttpClient.login(LOGIN_URL, 
+        				txtUsername.getText().toString(),
+        				txtPassword.getText().toString()))
+        		{
+        			Intent intent = new Intent(Intent.ACTION_VIEW,
+            				Uri.parse("http://www.google.com"));
+            		startActivity(intent);
+        		}
+        		else
+        		{
+        			Intent intent = new Intent(Intent.ACTION_VIEW,
+            				Uri.parse("http://www.facebook.com"));
+            		startActivity(intent);
+        		}
+        		
+        		
 //        		Intent intent = new Intent(Intent.ACTION_VIEW,
 //        				Uri.parse("http://www.google.com"));
 //        		startActivity(intent);
--- client/src/com/pieshare/util/PSHttpClient.java
+++ client/src/com/pieshare/util/PSHttpClient.java
@@ -2,36 +2,104 @@ package com.pieshare.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.util.ArrayList;
-	 
+import java.util.List;
+
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import android.util.Log;
+import android.widget.Toast;
 
 public class PSHttpClient {
+	private static final String TAG = "PSHttpClient";
+	
 	public static final int HTTP_TIMEOUT = 30 * 1000; // milliseconds
+	private static final String LOGIN_USER_FIELD = "username";
+	private static final String LOGIN_PASS_FIELD = "password";
 
-	private static HttpClient mHttpClient;
+	private static DefaultHttpClient mHttpClient;
+	private static List<Cookie> mCookies;
 
-    private static HttpClient getHttpClient() {
-        if (mHttpClient == null) {
-	        mHttpClient = new DefaultHttpClient();
-	        
-	        final HttpParams params = mHttpClient.getParams();
-	        HttpConnectionParams.setConnectionTimeout(params, HTTP_TIMEOUT);
-	        HttpConnectionParams.setSoTimeout(params, HTTP_TIMEOUT);
-	        ConnManagerParams.setTimeout(params, HTTP_TIMEOUT);
+	public static HttpClient getHttpClient() {
+		if (mHttpClient == null) {
+			mHttpClient = new DefaultHttpClient();
+		}
+		return mHttpClient;
+	}
+	
+	private static String convertStreamToString(java.io.InputStream is) {
+	    try {
+	        return new java.util.Scanner(is).useDelimiter("\\A").next();
+	    } catch (java.util.NoSuchElementException e) {
+	        return "";
 	    }
-	    return mHttpClient;
+	}
+
+	public static boolean login(String url, String username, String password) {
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+		boolean ret = false;
+
+		try {
+			HttpGet httpget = new HttpGet(url);
+
+				HttpPost httpost = new HttpPost(url);
+
+				List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+				nvps.add(new BasicNameValuePair(LOGIN_USER_FIELD, username));
+				nvps.add(new BasicNameValuePair(LOGIN_PASS_FIELD, password));
+
+				try {
+					httpost.setEntity(new UrlEncodedFormEntity(nvps));
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+				
+				HttpResponse	response = httpclient.execute(httpost);
+				HttpEntity entity = response.getEntity();
+				String res = convertStreamToString(entity.getContent());
+				Log.d(TAG, "Response: " + res);
+				ret = res.contains("Welcome");
+
+//				Log.d(TAG, "Post logon cookies:");
+//				List<Cookie> cookies = mHttpClient.getCookieStore().getCookies();
+//				if (cookies.isEmpty()) {
+//					Log.d(TAG, "None");
+//				} else {
+//					for (int i = 0; i < cookies.size(); i++) {
+//						Log.d(TAG, "- " + cookies.get(i).toString());
+//					}
+//				}
+				
+//				mCookies = cookies;
+		} catch (ClientProtocolException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			// When HttpClient instance is no longer needed,
+			// shut down the connection manager to ensure
+			// immediate deallocation of all system resources
+			httpclient.getConnectionManager().shutdown();
+		}
+		
+		return ret;
 	}
 
 }
